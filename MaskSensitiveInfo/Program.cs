bde819a19b519619d435a9ed44db9cdf88fe6347
Alesha Reed <aleshak83@gmail.com>
2020-03-17 21:38:03 -0500

    Add project files.

---
+++ MaskSensitiveInfo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MaskSensitiveInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a secret: ");
+            var secret = Console.ReadLine();
+            int length = secret.Length;
+            int allButLastFourLenth = secret.Substring(0, length - 4).Length;
+            var allButLastFour = new string('X', allButLastFourLenth);
+            var maskedSecret = secret.Substring(length - 4, 4);
+            Console.WriteLine(allButLastFour+maskedSecret);
+            
+        }
+    }
+}
